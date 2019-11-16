47e7034f64e95eb37edd1f2c12bf5def3b3d4c93
Saif Sultan <saifraikkonen@gmail.com>
2019-11-16 15:19:07 +0530

    service worker bug fix

--- sw.js
+++ sw.js
@@ -1,49 +1,48 @@
 const cacheName = 'cache-v1';
 const resourcesToPrecache = [
-  '/',
-  '/index.html',
-  '/budget icon.png',
-  '/css.bootstrap.min.css',
-  '/css/all.css',
-  '/css/main.css',
-  '/bootsrap.bundle.min.js',
-  '/jquery-3.3.1.min.js',
-  '/js/app.js'
+	'/',
+	'/index.html',
+	'/budget icon.png',
+	'/css.bootstrap.min.css',
+	'/css/all.css',
+	'/css/main.css',
+	'/bootsrap.bundle.min.js',
+	'/jquery-3.3.1.min.js',
+	'/js/app.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName)
-      .then(cache => {
-        return cache.addAll(resourcesToPrecache);
-      })
-    );
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(cacheName).then((cache) => {
+			return cache.addAll(resourcesToPrecache);
+		})
+	);
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {});
+
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then((cachedResponse) => {
+			return cachedResponse || fetch(event.request);
+		})
+	);
 });
 
-self.addEventListener('fetch', event=> {
-  event.respondWith(caches.match(event.request)
-    .then(cachedResponse => {
-      return cachedResponse || fetch(event.request);
-    });
-  );
+let deferredPrompt;
+self.addEventListener('beforeinstallprompt', (event) => {
+	event.preventDefault();
+	deferredPrompt = event;
+	btnAdd.style.display = 'block';
+
+	btn.addEventListener('click', (event) => {
+		deferredPrompt.prompt();
+		deferredPrompt.userChoice.then((choiceResult) => {
+			deferredPrompt = null;
+		});
+	});
 });
 
-self.addEventListener('beforeinstallprompt', event => {
-  event.preventDefault();
-  deferredPrompt = event;
-  btnAdd.style.display = 'block';
-  
-  btn.addEventListener('click, event => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      deferredPrompt = null;
-    });
-  });
-});
-
-self.addEventListener('appinstalled', event => {
-  app.logEvent('a2hs', 'installed');
+self.addEventListener('appinstalled', (event) => {
+	app.logEvent('a2hs', 'installed');
 });
