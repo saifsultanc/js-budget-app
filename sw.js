05fd136f53fc91a19f737162f33f228cbb99205b
Saif Sultan <saifraikkonen@gmail.com>
2019-11-16 15:11:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -27,8 +27,8 @@ self.addEventListener('fetch', event=> {
   event.respondWith(caches.match(event.request)
     .then(cachedResponse => {
       return cachedResponse || fetch(event.request);
-    })
-  };
+    });
+  );
 });
 
 self.addEventListener('beforeinstallprompt', event => {
